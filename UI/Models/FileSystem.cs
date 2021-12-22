24352856ee77116d6b507d24c5b3fbc4b409f666
svergut <anunnamedanonymous@gmail.com>
2021-12-22 09:50:28 +0500

    filter files

--- UI/Models/FileSystem.cs
+++ UI/Models/FileSystem.cs
@@ -1,6 +1,7 @@
 ﻿using Archivarius;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -10,15 +11,23 @@ namespace UI.Models
 {
     public class FileSystem
     {
+        public readonly static List<string> SupportedFileExtenstions = new List<string>() 
+        {
+            ".txt", ".jpg" //add jpeg/png?
+        };
+
         public static List<ArchivariusEntity> GetDirectoryContent(string path)
         {
-            var dirContent = Directory.GetFileSystemEntries(path).Select(entry =>
+            var dirContent = Directory.GetFileSystemEntries(path)
+            .Select(entry =>
             {
                 var file = new FileInfo(entry);
                 var extension = file.Exists ? file.Extension : null;
 
                 return new ArchivariusEntity(file.Name, file.FullName, extension);
-            }).ToList();
+            })
+            .Where(entry => entry.Extension == null || SupportedFileExtenstions.Contains(entry.Extension.ToLower()))
+            .ToList();
 
             return dirContent;
         }
